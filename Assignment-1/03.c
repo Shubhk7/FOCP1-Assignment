f7c6d3b3886a85592b057f340406438701a3c9ee
ShubhK7 <shubh.kashyap.7806delhi@hotmail.com>
2025-11-01 23:40:29 +0530

    Changes to be committed: 	modified:   Assignment-1/03.c 	deleted:    Assignment-2/01.exe 	deleted:    Assignment-2/10.exe 	deleted:    Assignment-2/11.exe 	deleted:    Assignment-2/13.exe 	deleted:    Assignment-2/14.exe 	deleted:    Assignment-2/15.exe 	deleted:    Assignment-2/16.exe 	deleted:    Assignment-2/17.exe 	deleted:    Assignment-2/18.exe

--- Assignment-1/03.c
+++ Assignment-1/03.c
@@ -1,3 +1,28 @@
 // Design a C program to subtract two integers without using the minus (-) operator, applying
 // bitwise operators. Highlight how such logic can be used in low-level arithmetic operations in
 // embedded systems or processors.
+#include <stdio.h>
+int sub(int a, int b)
+{
+    while (b !=0)
+    {
+        int borrow = (~a) & b;
+        a = a ^ b;
+        b = borrow << 1;
+    }
+    return a;
+}
+
+int main()
+{
+    int num1, num2;
+    printf("Enter two integers to subtract (num1 - num2): ");
+    printf("\nNum1: ");
+    scanf("%d", &num1);
+    printf("\nNum2: ");
+    scanf("%d", &num2);
+
+    printf("\n\nResult of %d - %d = %d\n", num1, num2, sub(num1, num2));
+
+    return 0;
+}
